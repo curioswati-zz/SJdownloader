1b6406ced18591c8c4874aacdd7bb8f4988c57cc
Swati Jaiswal <jaiswalswati94@gmail.com>
2014-10-31 22:04:05 +0530

    updates comments and returns errors instead of printing them

--- downloader/downloader_script.py
+++ downloader/downloader_script.py
@@ -1,26 +1,24 @@
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
-''This script is used to download images from webpages
-It Download all the images available on the input url page,
-It takes input as a url.
+''This script is used to download content from webpages
+It Download the content from provided url.
 
 It imports:
   -urllib
-  -os
   
 It defines:
   -main
   -extract_name
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 '''Required modules'''
-import os, urllib
+import urllib
 
 def extract_name(url):
     """
-    Extracts the name of the image form provided url.
+    Extracts the name of the content item from provided url.
     """
     name = ""                         
     for ch in url[-1::-1]: 
-        if ch == "/":                                                 #extracting the name of image from last field separated by "/"
+        if ch == "/":                                                 #extracting from last field separated by "/"
             break
         name+=ch
         
@@ -28,17 +26,17 @@ def extract_name(url):
             
 def main(urls, path):
 
-    print "Downloading into "+path+" ..."
+    #print "Downloading into "+path+" ..."
     for url in urls:
         name = extract_name(url)
-        print name
+        #print name
         try:
             data = urllib.urlopen(url).read()
             save_file = open(path+"/"+name, 'wb')
             save_file.write(data)
             save_file.close()
         except:
-            print "The connection could not establish"
+            return "The connection could not establish."
             
 if __name__ == "__main__":
     main(url,dir_)
