e7a9528bb48bd7a8962c601ca0f2b9dec06568a3
swati12jaiswal <jaiswalswati94@gmail.com>
2014-11-27 14:46:35 +0530

    implements downloading in a thread implements progress bar as class

--- downloader/downloader_script.py
+++ downloader/downloader_script.py
@@ -12,71 +12,158 @@ It defines:
 '''Required modules'''
 import urllib
 import wx
+import time
+import os
+from threading import Thread
+#from wx.lib.pubsub import setuparg1
+from wx.lib.pubsub import pub
 from wx.lib.agw import pygauge as PG
 
-def extract_name(url):
-    """
-    Extracts the name of the content item from provided url.
-    """
-    name = ""                         
-    for ch in url[-1::-1]: 
-        if ch == "/":                                                 #extracting from last field separated by "/"
-            break
-        name+=ch
-        
-    return name[-1::-1]
+#written for cutting the name of file from url, replaced by os.path.basename in `TestThread`
+##def extract_name(url):
+##    """
+##    Extracts the name of the content item from provided url.
+##    """
+##    name = ""                         
+##    for ch in url[-1::-1]: 
+##        if ch == "/":                                                 #extracting from last field separated by "/"
+##            break
+##        name+=ch
+##        
+##    return name[-1::-1]
+
+class TestThread(Thread):
+    '''
+    TestThread class to run the thread of downloading.
+    '''
+    def __init__(self,urls,path,stop,rename):
+        self.rename = rename
+        self.urls = urls
+        self.path = path
+        self.stop = stop
+        Thread.__init__(self)
+        self.start()
+
+    def run(self):
+        print "Downloading into "+self.path+"..."
+        for url in self.urls:
+            f_name = os.path.basename(url)
+
+            #Check if file exists and want to rename
+            if os.path.exists(f_name) and not self.rename:
+                return
+            else:
+                count = 1
+                while True:
+                    if os.path.exists(f_name):
+                           tmp, ext = os.path.splitext(f_name)
+                           cnt = "(%s)" % count
+                           f_name = tmp+cnt+ext
+                           count += 1
+                           print f_name
+                    else:
+                        break
             
+            try:
+                data = urllib.urlopen(url).read()
+#                req = requests.get(url,stream=True)
+                print f_name
+                save_file = open(self.path+"/"+f_name, 'wb')
+                save_file.write(data)
+                dl_size = len(data)
+                save_file.close()
+                wx.CallAfter(pub.sendMessage,"update",msg=dl_size)
+            except IOError as e:
+                return e
+                return "The connection could not establish."
+
+class Progress(wx.Gauge):
+    '''
+    Progress class to implement the updation of progress bar with
+    an ongoing process of downloading.
+    '''
+    def __init__(self,progress,total_size):
+        
+        progress_bar = progress
+        #progress bar attributes
+        progress_bar.SetBarColor([wx.Colour(162,255,178),wx.Colour(159,176,255)])
+        progress_bar.SetBackgroundColour(wx.CYAN)
+        progress_bar.SetBorderColor(wx.BLACK)
+        progress_bar.SetBorderPadding(2)
+        progress_bar.SetRange(total_size)
+
+        #updating bar
+        pub.subscribe(self.updateProgress, "update")
+
+    def updateProgress(self, msg):
+        print "update:",msg
+        progress_bar.SetValue(msg)
+
 def main(urls, path,progress_bar):
+    '''
+    Main module to call the TestThread for downloading and
+    Progress to update the progress bar. It also calculates the size
+    of file before downloading.
+    '''    
     #When called with direct url
-    
     stop = False
-    print urls
     if (not(urls[0].startswith("http"))
         and ".html" not in urls[0]):
         return "Invalid url"
-
-    total_size = 0
-
-    for url in urls:
-        connection = urllib.urlopen(url)
-        meta = connection.info()
-        try:
-            size = meta.getheaders("content-length")[0]
-            total_size += int(size)
-        except IndexError:
-            size = len(connection.read())
-            total_size += size
-
-    percent = total_size / len(urls)
-    total_size /= float(1024**2)
-    print str(total_size) + "Mb selected for download"
-    #progress bar attributes
-#    progress_bar.SetValue([0,total_size])
-#    progress_bar.SetBarColor([wx.Colour(162,255,178),wx.Colour(159,176,255)])
-#    progress_bar.SetBackgroundColour(wx.WHITE)
-#    progress_bar.SetBorderColor(wx.BLACK)
-#    progress_bar.SetBorderPadding(2)
-#    progress_bar.SetDrawValue(draw=True, drawPercent=True, font=wx.SMALL_FONT, colour=wx.BLUE)
-#    progress_bar.Update([percent,0],2000)
-
-    
-    print "Downloading into "+path+" ..."
-    for url in urls:
-        name = extract_name(url)
-        try:
-            data = urllib.urlopen(url).read()
-            save_file = open(path+"/"+name, 'wb')
-            save_file.write(data)
-            save_file.close()
-            print name
-
-#            download_size = len(data)
-#            percent = download_size / float(total_size)           
-            if stop:
-                break
-        except IOError as e:
-            return e
-            return "The connection could not establish."
-            
+    #-----------------------------------------------------------------------------------------------
+    #Fetching file size before download
+    try:
+        total_size = 0
+        for url in urls:
+            connection = urllib.urlopen(url)
+            meta = connection.info()
+            try:
+                size = float(meta.getheaders("content-length")[0])
+                total_size += size
+            except IndexError:
+                size = len(connection.read())
+                total_size += size
+    except IOError:
+        return "The connection could not establish"
+    #-----------------------------------------------------------------------------------------------
+    #replaced by dl_size in `Thread`
+    #update_value = total_size / len(urls)
+    print_size = total_size/float(1024**2)
+    print str(print_size) + "Mb selected for download"
+    #-----------------------------------------------------------------------------------------------
+    #update the progress bar
+    Progress(progress_bar,total_size)
+    #Start thread
+    TestThread(urls, path,stop,True)
+                
 if __name__ == "__main__":
     main(url,dir_)
+
+
+
+
+
+
+
+
+
+    
+#-----------------------------------------------------------------------------------------------
+#implement when requests downloaded
+#writing byte by byte
+##                for byte in req.iter_content(chunk_size=1024):
+##                    if self.stop:
+##                        break
+##                    if byte:
+##                        save_file.write(byte)
+##                        save_file.flush()
+##                    dl_size = len(byte)
+##
+##                    if dl_size < total_size:
+##                        wx.CallAfter(pub.sendMessage,"update",msg=dl_size)
+##
+##    progress_bar.SetValue([0,total_size])   
+##    progress_bar.SetDrawValue(draw=True, drawPercent=True, font=wx.SMALL_FONT, colour=wx.BLUE)
+
+##            download_size = len(data)
+#            percent = download_size / float(total_size)           
