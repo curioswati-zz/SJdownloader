e0399a8deb159626b978d7d572343ecad8d5f1c7
swati12jaiswal <jaiswalswati94@gmail.com>
2014-11-28 15:12:17 +0530

    adds reference to join_path

--- downloader/change_config.py
+++ downloader/change_config.py
@@ -1,10 +1,9 @@
 import os
-def opj(path):
-     return apply(os.path.join, tuple(path.split('/')))
+from join_path import opj
 #------------------------------------------------------
     
 def main(dir_,filters):
-     dir_file = open('/home/dc-19/Documents/GitHub/Downloader/downloader/config.txt','w')
+     dir_file = open(opj('config.txt'),'w')
      dir_file.write(dir_)
      dir_file.write("\n"+filters)
      dir_file.close()
