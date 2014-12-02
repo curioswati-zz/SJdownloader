54306eedf74a63c8957f21a7e8e4eaeaa02c9788
swati12jaiswal <jaiswalswati94@gmail.com>
2014-12-03 00:42:14 +0530

    adds history and option values

--- downloader/change_config.py
+++ downloader/change_config.py
@@ -2,8 +2,11 @@ import os
 from join_path import opj
 #------------------------------------------------------
     
-def main(dir_,filters):
+def main(dir_,filters,option,history):
+     print history
      dir_file = open(opj('config.txt'),'w')
-     dir_file.write(dir_)
-     dir_file.write("\n"+filters)
+     dir_file.write('PATH = '+dir_)
+     dir_file.write("\n"+'FILTER = '+filters)
+     dir_file.write("\n"+'OPTION = '+option)
+     dir_file.write("\n"+'HISTORY = '+history)
      dir_file.close()
