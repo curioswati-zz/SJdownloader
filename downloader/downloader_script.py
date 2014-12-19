faab0508c38dc765adc2df89a3b9331dd06ee1cc
swati12jaiswal <jaiswalswati94@gmail.com>
2014-12-19 16:57:03 +0530

    adds segmentation

--- downloader/downloader_script.py
+++ downloader/downloader_script.py
@@ -14,7 +14,7 @@ It defines:
     -updateProgress
 '''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
 '''Required modules'''
-import urllib
+import urllib, requests
 import wx
 import time
 import os
@@ -48,7 +48,8 @@ def read_config():
         rename = data[radio_point+9:end_point].strip()
 
     #Trailing extra whitespaces
-    rename = sanitize_string(rename)
+    if rename:
+        rename = sanitize_string(rename)
 
 #--------------------------------------------------------------------------
 class TestThread(Thread):
@@ -64,66 +65,96 @@ class TestThread(Thread):
         self.start()
 
     def run(self):
-    	try:
-	        time.sleep(1)
-	        global rename, FILE_EXIST
 
-	        read_config()
-	        dl_size = 0
+        global progress
 
-	        print "Downloading into "+self.path+"..."
-                print self.path
-
-	        for url in self.urls:
-
-	            cancel = False
-	            
-	            f_name = os.path.basename(url)
-	            #Check if file exists and want to rename
-	            FILE_EXIST = os.path.exists(self.path+"/"+f_name)
-
-	            #IF default option is to cancel download, when already exist
-	            if FILE_EXIST and rename == 'Cancel':
-	                print f_name+" already exists, Canceling download"
-	                cancel = True
-	            
-	            #If default option is to rename new download, when already exist
-	            if FILE_EXIST and rename == 'Rename':
-	                count = 1
-	                old_f_name = f_name
-	                while True:
-	                    if os.path.exists(self.path+"/"+f_name):
-	                           tmp, ext = os.path.splitext(f_name)
-	                           cnt = "(%s)" % count
-	                           f_name = tmp+cnt+ext
-	                           count += 1
-	                           print f_name
-	                    else:
-	                        break
-	                print old_f_name+" already exist, renaming to "+f_name
-
-	            #If default option is to remove old download, when already exist                        
-	            elif FILE_EXIST and rename == 'Replace':
-	                os.remove(self.path+"/"+f_name)
-	                print f_name+" already exists, removing older one."
-
-	            if not cancel:
-	                try:
-	                    data = urllib.urlopen(url).read()
-	    #                req = requests.get(url,stream=True)
-	                    save_file = open(self.path+"/"+f_name, 'wb')
-	                    save_file.write(data)
-	                    dl_size = len(data)
-	                    save_file.close()
-	                    print f_name
-	                    wx.CallAfter(pub.sendMessage,"update",msg=dl_size)
-	#                    self.parent.Layout()
-	                except IOError:
-	                    return "The connection could not establish."
-	                except Exception as e:
-	                    return e
+        #Fetching file size before download
+        try:
+            total_size = 0
+            for url in self.urls:
+                connection = urllib.urlopen(url)
+                meta = connection.info()
+                try:
+                    size = float(meta.getheaders("content-length")[0])
+                    total_size += size
+                except IndexError:
+                    size = len(connection.read())
+                    total_size += size
+        except IOError:
+            return "The connection could not establish"
         except Exception as e:
-            print e
+            return e
+
+        #-----------------------------------------------------------------------------
+        print_size = total_size/float(1024**2)
+        print str(print_size) + "Mb selected for download"
+        print total_size
+        progress.SetRange(total_size)
+        #-----------------------------------------------------------------------------
+
+        try:
+            time.sleep(1)
+            global rename, FILE_EXIST
+            
+            read_config()
+            print "Downloading into "+self.path+"..."
+
+            for url in self.urls:
+
+                cancel = False
+                dl_size = 0
+	            
+                f_name = os.path.basename(url)
+                #Check if file exists and want to rename
+                FILE_EXIST = os.path.exists(self.path+"/"+f_name)
+
+                #IF default option is to cancel download, when already exist
+                if FILE_EXIST and rename == 'Cancel':
+                    print f_name+" already exists, Canceling download"
+                    cancel = True
+	            
+                #If default option is to rename new download, when already exist
+                if FILE_EXIST and rename == 'Rename':
+                    count = 1
+                    old_f_name = f_name
+                    while True:
+                        if os.path.exists(self.path+"/"+f_name):
+                               tmp, ext = os.path.splitext(f_name)
+                               cnt = "(%s)" % count
+                               f_name = tmp+cnt+ext
+                               count += 1
+                               print f_name
+                        else:
+                            break
+                    print old_f_name+" already exist, renaming to "+f_name
+
+                #If default option is to remove old download, when already exist                        
+                elif FILE_EXIST and rename == 'Replace':
+                    os.remove(self.path+"/"+f_name)
+                    print f_name+" already exists, removing older one."
+
+                if not cancel:
+                    try:
+                        req = requests.get(url,stream=True)
+                        print 'req:',req
+                        save_file = open(self.path+"/"+f_name, 'wb')
+                        for byte in req.iter_content(chunk_size=1024):
+                            if self.stop:
+                                break
+                            if byte:
+                                save_file.write(byte)
+                                save_file.flush()
+                            dl_size += len(byte)
+                            wx.CallAfter(pub.sendMessage,"update",msg=dl_size)
+                        print f_name
+#                        wx.CallAfter(pub.sendMessage,"update",msg=dl_size)
+  #                      self.parent.Layout()
+                    except IOError:
+                        return "The connection could not establish."
+                    except Exception as e:
+                        return e
+        except Exception as e:
+            print 'Exception:',e
 
 #--------------------------------------------------------------------------------------------------
 def main(urls, path,progress_bar):
@@ -141,35 +172,11 @@ def main(urls, path,progress_bar):
         and ".html" not in urls[0]):
         return "Invalid url"
     #-----------------------------------------------------------------------------
-    #Fetching file size before download
-    try:
-        total_size = 0
-        for url in urls:
-            connection = urllib.urlopen(url)
-            meta = connection.info()
-            try:
-                size = float(meta.getheaders("content-length")[0])
-                total_size += size
-            except IndexError:
-                size = len(connection.read())
-                total_size += size
-    except IOError:
-        return "The connection could not establish"
-    except Exception as e:
-        return e
-
-    #-----------------------------------------------------------------------------
-    print_size = total_size/float(1024**2)
-    print str(print_size) + "Mb selected for download"
-    #-----------------------------------------------------------------------------
-
     #progress bar attributes
     progress_bar.SetBarColor([wx.Colour(162,255,178),wx.Colour(159,176,255)])
     progress_bar.SetBackgroundColour(wx.CYAN)
     progress_bar.SetBorderColor(wx.BLACK)
     progress_bar.SetBorderPadding(2)
-    print total_size
-    progress_bar.SetRange(total_size)
 
     #updating bar
     pub.subscribe(update_progress, "update")
@@ -182,9 +189,20 @@ def update_progress(msg):
     updates the progress bar
     '''
     global progress
-    print msg
-    progress.Update(msg,100)
+    print 'msg:',msg
+    progress.SetValue(msg)
+    progress.Refresh()
+#    progress.Update(msg,50)
 
 #--------------------------------------------------------------------------------------------------
 if __name__ == "__main__":
     main(url,dir_)
+
+#urllib implementation
+#{
+    # data = urllib.urlopen(url).read()
+    # save_file = open(self.path+"/"+f_name, 'wb')
+    # save_file.write(data)
+    # dl_size = len(data)
+    # save_file.close()
+#}
