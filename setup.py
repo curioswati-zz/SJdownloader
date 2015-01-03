5eaf9415762aa7dfeb14c04d7f6f45277c29439c
swati12jaiswal <jaiswalswati94@gmail.com>
2015-01-03 11:53:24 +0530

    updated

--- setup.py
+++ setup.py
@@ -2,11 +2,14 @@
 from distutils.core import setup
 
 setup(
-	name='SJdownloader',
+	name='SJDownloader',
 	version='1.0.0',
 	description='Desktop application for internet content downloading.',
+	long_description=open('README.md').read(),
 	author='Swati Jaiswal',
 	author_email='jaiswalswati94@gmail.com',
-	url='https://github.com/swati-jaiswal/Downloader'
-	packages=['downloader']
+	url='https://github.com/swati-jaiswal/Downloader',
+	packages=['downloader'],
+	data_files=[('Icons', ['Icons/folder.png','Icons/Logo.png','Icons/new.png','Icons/package.png','Icons/pref.png']),
+                ('config', ['config/config.txt','config/content.txt'])],
 	)
