dbf6e93a89c1e768a1c673730f85b90cec3adcf9
swati12jaiswal <jaiswalswati94@gmail.com>
2015-01-04 02:41:00 +0530

    updates docs and renamed

---
+++ downloader/SJdownloader.py
@@ -0,0 +1,37 @@
+"""
+The downloader main module.
+It creates a GUI window with the help of Mypanel classes.
+It import:
+    -wx
+    -Mypanel    
+"""
+import wx
+from class_Mypanel import Mypanel
+
+def main():
+    '''
+    The main function which creates the window and box objects and uses
+    Mypanel class for creating widgets and binding events.
+    '''
+    app = wx.App()
+
+    #window object
+    win = wx.Frame(None,title = "SJdownloader",size=(575,420))
+
+    #box object
+    bkg = wx.Panel(win)
+    
+    #packing the box
+    Mypanel(bkg,win)
+
+    #show the window
+    win.Show()
+
+    #execute the loop for maintaining the window
+    app.MainLoop()
+
+    #destroy window
+    #win.Destroy()
+
+if __name__ == '__main__':
+    main()
