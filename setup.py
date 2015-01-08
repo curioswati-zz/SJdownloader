47c1df06135ce5ea25e915286a8ab442dbd7512d
swati12jaiswal <jaiswalswati94@gmail.com>
2015-01-08 18:13:08 +0530

    updates with license information

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 #setup.py
 from distutils.core import setup
+import py2exe
+import os
+
+images = ['downloader/Icons/'+file for file in os.listdir('downloader/Icons')]
+configs = ['downloader/config/'+file for file in os.listdir('downloader/config')]
 
 setup(
-	name='SJDownloader',
+	name='SJdownloader',
 	version='1.0.0',
 	description='Desktop application for internet content downloading.',
+	license=open('LICENSE.txt').read()
 	long_description=open('README.md').read(),
 	author='Swati Jaiswal',
 	author_email='jaiswalswati94@gmail.com',
 	url='https://github.com/swati-jaiswal/Downloader',
 	packages=['downloader'],
-	data_files=[('Icons', ['Icons/folder.png','Icons/Logo.png','Icons/new.png','Icons/package.png','Icons/pref.png']),
-                ('config', ['config/config.txt','config/content.txt'])],
+	data_files=[('Icons', images),
+                ('config', configs)],
 	)
